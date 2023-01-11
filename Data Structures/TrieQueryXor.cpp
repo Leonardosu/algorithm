2883df7700219e3189eff4e160282473330c8204
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-01-11 19:38:00 -0300

    white spaces

--- Data Structures/TrieQueryXor.cpp
+++ Data Structures/TrieQueryXor.cpp
@@ -1,13 +1,5 @@
 #include "bits/stdc++.h" // Leonardo Su
-#define f first
-#define s second
-#define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define sz(x) (int)(x).size()
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
 /*
 	Given a N queries.
 	There are three types.
@@ -16,29 +8,22 @@ typedef vector<int> vi;
 	? x . Print the max xor you can get with x and a number form your multiset. 
 		  max value x ^ y, y in multiset
 */
-
+z
 #define N 32
 #define S 2
-struct node
-{
-	int val;
-	int qtd;
+struct node {
+	int val, qtd;
 	node *prox[S];
 
-	node()
-	{
+	node() {
 		qtd = val = 0;
 		for(int i = 0; i < S; i++) prox[i] = NULL;
 	}
 
-	void Insert(int number) // Insert a new number
-	{
+	void Insert(int number) {
 		node *curr = this;
-
-		for(int i=N-1;i>=0;--i)
-		{
+		for(int i=N-1;i>=0;--i) {
 			bool bit = (number & (1<<i));
-
 			if(curr->prox[bit] == NULL)
 				curr->prox[bit] = new node();
 
@@ -50,14 +35,10 @@ struct node
 		curr->val = number;
 	}
 
-	void Remove(int number) // Remove a number
-	{
+	void Remove(int number) {
 		node *curr = this;
-
-		for(int i=N-1;i>=0;--i)
-		{
+		for(int i=N-1;i>=0;--i) {
 			bool bit = number & (1<<i);
-
 			curr->qtd--;
 			curr = curr->prox[bit];
 		}
@@ -65,48 +46,21 @@ struct node
 		curr->qtd--;
 	}
 
-	int query(int number) // max xor with this number
-	{
+	int query(int number) { // max xor with this number
 		node *curr = this;
-
-		for(int i=N-1;i>=0;--i)
-		{
+		for(int i=N-1;i>=0;--i) {
 			bool bit = number & (1<<i);
-
 			if(curr->prox[1-bit] != NULL && curr->prox[1-bit]->qtd > 0)
 				curr = curr->prox[1-bit];
-
 
 			else if(curr->prox[bit] != NULL && curr->prox[bit]->qtd > 0)
 				curr = curr->prox[bit];
 		}
-
 		return  number ^ curr->val;
 	}
-
 };
 
 node *tree = new node();
+int main() {
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	
-	int n;
-	cin>>n;
-	tree->Insert(0);
-
-	while(n--)
-	{
-		char t;
-		int x;
-		cin>>t>>x;
-
-		if(t == '+')
-			tree->Insert(x);
-		else if(t == '-')
-			tree->Remove(x);
-		else		
-			cout<<tree->query(x)<<"\n";		
-	}
 }
