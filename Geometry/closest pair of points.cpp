4f165f09d70acabb47ba72a75df873f1c7ec5e23
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-04-16 00:09:24 -0300

    update closest points

--- Geometry/closest pair of points.cpp
+++ Geometry/closest pair of points.cpp
@@ -3,65 +3,59 @@
 #define s second
 #define pb push_back
 #define sz(x) (int)(x).size()
-#define ALL(x) x.begin(),x.end()
+#define all(x) x.begin(),x.end()
 #define present(c, x) (c.find(x) != c.end())
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-struct point {   
-    ll x, y;    
-	int id;    
-    point(ll x=0,ll y=0, int id=0) : x(x), y(y), id(id){}
+struct pt {
+    using P = pt;
+    using T = long long;
+
+    T x, y; 
+    int id;
+    explicit pt(T x=0, T y=0) : x(x), y(y) {}
+    bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+    bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+    P operator+(P p) const { return P(x+p.x, y+p.y); }
+    P operator-(P p) const { return P(x-p.x, y-p.y); }
+    P operator/(T d) const { return P(x/d, y/d); }
+    T dot(P p) const { return x*p.x + y*p.y; }
+    T dist2() const { return x*x + y*y; }
+    double dist() const { return sqrt((double)dist2()); }
+    double angle() const { return atan2(y, x); }
+    P unit() const { return *this/dist(); } // makes dist()=1
+    P perp() const { return P(-y, x); } // rotates +90 degrees
 };
 
-ll dist(const point &a, const point &b) {
-	return (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
-}
-bool cmp_y(const point &a, const point &b) {
-	return a.y < b.y;
-}
-
-ll minDist = LLONG_MAX;
-pii best;
-void upd(const point &a, const point &b) {
-	ll curr = dist(a, b);
-	if(curr < minDist) {
-		minDist = curr;
-		best = {a.id, b.id};
+pair<ll, pair<pt, pt>> ret;
+void upd(const pt &a, const pt &b) {
+	ll curr = (a - b).dist2();
+	if(curr < ret.f) { //min dist
+		ret.f = curr;
+		ret.s = {a, b};
 	}
 }
 
-vector<point> p, tmp;
-void solve(int l, int r) {
-	if(r - l <= 3) {
-		for(int i=l;i<r;++i) 
-			for(int j=i+1;j<r;++j)
-				upd(p[i], p[j]);
-		sort(p.begin() + l, p.begin() + r, cmp_y);
-		return;
+pair<pt, pt> closest(vector<pt> v) {
+	assert(sz(v) > 1);
+
+	set<pt> S;
+	sort(all(v), [](pt a, pt b) { return a.y < b.y; });
+	ret = {LLONG_MAX, {pt(), pt()}};
+	int j = 0;
+	for (pt p : v) {
+		pt d{1 + (ll)sqrt(ret.first), 0};
+		while (v[j].y <= p.y - d.x) S.erase(v[j++]);
+		auto lo = S.lower_bound(p - d), hi = S.upper_bound(p + d);
+		for (; lo != hi; ++lo)
+			upd(*lo, p);
+		
+		S.insert(p);
 	}
-
-	int mid = (l + r)/2;
-	int midx = p[mid].x;
-
-	solve(l, mid);
-	solve(mid, r);
-
-	merge(p.begin() + l, p.begin() + mid, 
-			p.begin() + mid, p.begin() + r, 
-				tmp.begin(), cmp_y);
-    copy(tmp.begin(), tmp.begin() + r - l, p.begin() + l);
-
-    int tsz = 0;
-    for (int i = l; i < r; ++i) {
-        if (abs(p[i].x - midx) < minDist) {
-            for (int j = tsz - 1; j >= 0 && p[i].y - tmp[j].y < minDist; --j)
-            	upd(p[i], tmp[j]);
-            tmp[tsz++] = p[i];
-        }
-    }
+	return ret.second;
 }
 
 int main() {
@@ -69,19 +63,14 @@ int main() {
 
     int n;
     cin>>n;
-    for(int i=0;i<n;++i) {
-    	int x, y;
-    	cin>>x>>y;
-    	p.pb({x, y, i});
+    vector<pt> points(n);
+    int i = 1;
+    for(auto &p : points) {
+        cin>>p.x>>p.y;
+        p.id = i++;
     }
     
-    sort(ALL(p), [&](const point &a, const point &b){
-    	if(a.x == b.x) return a.y < b.y;
-    	return a.x < b.x;
-    });
-
-    tmp.resize(n);
-    solve(0, n);
-    cout<<minDist<<"\n";
-    cout<<best.f<<" "<<best.s<<"\n";
+    closest(points);
+    cout<<ret.f<<"\n";
+    cout<<ret.s.f.id<<' '<<ret.s.s.id<<'\n';
 }
